05f97ff6ea6213efec1a74d4716f8d60cfd7fe50
Tran Huu Phuoc (phuocth) <phuocth@solarbk.vn>
2022-02-17 15:09:29 +0700

    template tailwindcss jit v3 typescript eslint prettier

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+export const App = () => {
+    return (
+        <div className="m-auto h-screen text-center">
+            <h2 className="text-2xl font-bold text-[red]">Welcome</h2>
+            <h4 className="text-green-500">Template tailwindcss jit v3 typescript</h4>
+            <a
+                className="block text-[blue]"
+                href="https://github.com/phuockaito/template-tailwindcss-jit-v3-typescript"
+            >
+                Download template
+            </a>
+        </div>
+    );
+};
---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import ReactDOM from "react-dom";
+import "./styles/tailwind.css";
+import { App } from "./App";
+ReactDOM.render(<App />, document.getElementById("root"));
